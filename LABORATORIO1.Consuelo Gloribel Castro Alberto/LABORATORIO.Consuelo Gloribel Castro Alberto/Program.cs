eca629162e57513f0a5d6f5ca915dc875e58e99a
Gloribel010 <125325837+Gloribel010@users.noreply.github.com>
2023-03-01 22:05:19 -0600

    Add files via upload

---
+++ LABORATORIO1.Consuelo Gloribel Castro Alberto/LABORATORIO.Consuelo Gloribel Castro Alberto/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using LABORATORIO.Consuelo_Gloribel_Castro_Alberto.Entidades.Negocio;
+using LABORATORIO.Consuelo_Gloribel_Castro_Alberto.Entidades;
+
+ClsPersona Clspersona = new ClsPersona();
+Persona persona = new Persona();
+
+Console.WriteLine("Bienvenido/a");
+Console.WriteLine("Ingresar su Nombre: ");
+persona.nombre = (Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingresar  su edad: ");
+persona.edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingresar su Sexo: ");
+persona.sexo = (Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingresar su Peso: ");
+persona.peso = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Ingresar su Altura: ");
+persona.altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(" ");
+
+Clspersona.PesodePersonas(persona);
+
+
+Console.WriteLine(" ");
+
+Clspersona.MayordeEdad(persona);
+
+
+Console.ReadKey();
